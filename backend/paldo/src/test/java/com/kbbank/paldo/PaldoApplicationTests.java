e989580319dc9e8824a738b2a2edc4c4552611fa
jaeseok-go <gojeasuk2@naver.com>
2021-10-18 16:13:12 +0900

    [ADD] portfolio-service initialize

---
+++ backend/paldo/src/test/java/com/kbbank/paldo/PaldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbbank.paldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaldoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
