0d292a40d16385ead1cdbd6381e9bac9a1b399da
박수아 <xlalfskdl@naver.com>
2021-10-19 09:50:08 +0900

    [ADD] menu nav

---
+++ frontend/src/components/common/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="nav">
+    <input type="checkbox" class="nav-checkbox" id="nav-toggle" />
+    <label for="nav-toggle" class="nav-button">
+      <span class="nav-icon">&nbsp;</span>
+    </label>
+    <div class="nav-bg">&nbsp;</div>
+    <div class="nav-body">
+      <ul class="nav-list">
+        <li class="nav-item">
+          <a class="nav-link">
+            <span><i class="fas fa-search"></i></span>포트폴리오
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">
+            <span><i class="fas fa-car"></i></span>지출관리
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">
+            <span><i class="fas fa-car"></i></span>구독관리
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">
+            <span><i class="fas fa-car"></i></span>금융상품추천
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    isShow() {
+      if (this.$route.name !== "Main") {
+        return false;
+      }
+      return true;
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style></style>
