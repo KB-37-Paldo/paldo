b08ed18285bcd50a5be8617de9354d327d16ec27
Jeongm1n <qll4359@naver.com>
2021-10-19 15:47:22 +0900

    [UPDATE] Budget

--- frontend/src/components/budget/BudgetBottomSection.vue
+++ frontend/src/components/budget/BudgetBottomSection.vue
@@ -5,13 +5,14 @@
             390,000원 남음
         </div>
         <v-btn elevation="2">예산 설정</v-btn>
-
+        <vue-gauge :refid="'type-unique-id'" :option="{'needleValue': 30, 'arcDelimiters': 99}"></vue-gauge>
     </div>
 </template>
 
 <script>
+import VueGauge from 'vue-gauge';
   export default {
-
+      components: {VueGauge}
   }
 </script>
 
