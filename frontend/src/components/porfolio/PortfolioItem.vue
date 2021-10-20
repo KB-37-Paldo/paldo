4734720feadb481be2a7c595784828bc72fb55be
박수아 <xlalfskdl@naver.com>
2021-10-20 14:31:34 +0900

    [ADD] FE portfolio chart basic

--- frontend/src/components/porfolio/PortfolioItem.vue
+++ frontend/src/components/porfolio/PortfolioItem.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <div class="mt__ten title__font">
+      <span>
+        전국구님의
+      </span>
+      <span class="block mt__five">
+        현재 자산 포트폴리오
+      </span>
+      <PortfolioChart/>
+    </div>
+  </div>
 </template>
 
 <script>
+import PortfolioChart from './PortfolioChart.vue'
 export default {
+  components: { PortfolioChart },
 
 }
 </script>
