02fc8d4938c190ffaaebb6c4bddc9010e6fe37c0
박수아 <xlalfskdl@naver.com>
2021-10-20 15:48:45 +0900

    [ADD] FE portfolio sidebar base css

---
+++ frontend/src/components/porfolio/PortfolioSideBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mt__ten center pointer portfolio__sidebar">
+    <div class="portfolio__sidebar__div">
+      <div class="mr__ten portfolio__sidebar__item" @click="goPortHome" :class="[toActive === 'PortfolioPage'? 'active' : '']">조회</div>
+      <div class="mr__ten portfolio__sidebar__item" @click="goProductRecom" :class="[toActive === 'PortfolioPage'? 'active' : '']">추천</div>
+      <div
+      class="portfolio__sidebar__item"
+      @click="goPortShare" :class="[toActive === 'PortfolioPage'? 'active' : '']">공유</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fromActive:'',
+      toActive: ''
+    }
+  },
+  watch: {
+    $route(to,from) {
+			this.fromActive = from.name;
+			this.toActive = to.name;
+      console.log(this.fromActive, this.toActive)
+      console.log(this.toActive)
+		}
+  },
+  methods : {
+    goPortHome() {
+      this.$router.push({
+        name:'PortfolioPage'
+      })
+    },
+    goProductRecom() {
+      this.$swal('잠시생성으로바꿈')
+      this.$router.push({
+        name:'PortfolioCreatePage'
+      })
+    },
+    goPortShare() {
+      this.$swal('아직안만들었어용')
+      this.$router.push({
+        name:'PortfolioPage'
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
