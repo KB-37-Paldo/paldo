6e16506e2839e54336d11864dfbc79d2c5812f52
박수아 <xlalfskdl@naver.com>
2021-10-21 17:31:43 +0900

    [FIX] jenkinsfile

--- src/views/portfolio/PortfolioSharePage.vue
+++ src/views/portfolio/PortfolioSharePage.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="bgColor__lightgray">
-    <div class="portfolio__div">
-      <div class="mt__ten title__font">
-        <span>
-          포트폴리오 공유
-        </span>
-        
-        <span class="block mt__five sub__font">
-          {{age}}대 TOP 포트폴리오
-        </span>
-        <PortfolioChart portType="recommend"/>
+  <div class="portfolio__div">
+    <div class="mt__ten title__font">
+      <span>
+        포트폴리오 셰어링
+      </span>
+      <div class="bgColor__lightgray mt__minusTen">
+        <PortfolioShareItem shareType="age"/>
+        <PortfolioShareItem shareType="money"/>
+        <PortfolioShareItem shareType="investType"/>
       </div>
     </div>
     <PortfolioSideBar/>
@@ -17,15 +15,11 @@
 </template>
 
 <script>
-import PortfolioChart from '../../components/porfolio/PortfolioChart.vue'
+import PortfolioShareItem from '../../components/porfolio/PortfolioShareItem.vue'
 import PortfolioSideBar from '../../components/porfolio/PortfolioSideBar.vue'
 export default {
-  components: { PortfolioSideBar, PortfolioChart },
-  data() {
-    return {
-      age:20
-    }
-  },
+  components: { PortfolioSideBar, PortfolioShareItem },
+  
 
 }
 </script>
