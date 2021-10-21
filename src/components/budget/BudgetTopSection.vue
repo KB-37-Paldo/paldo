c11714761633e896545132dbbf42230690b3b9c6
Jeongm1n <qll4359@naver.com>
2021-10-21 13:48:22 +0900

    [ADD] Spending Page

--- src/components/budget/BudgetTopSection.vue
+++ src/components/budget/BudgetTopSection.vue
@@ -1,28 +1,27 @@
 <template>
+  <div>
     <div>
-        <div>
-            <div>
-                <span style="font-size: 25px">지출 </span>
-                <span style="font-size: 30px">600,000원</span>
-            </div>
-            <div>
-                <span style="font-size: 25px">수입 </span>
-                <span style="font-size: 30px">100,000원</span>
-            </div>
-            <div class="anaysis-button-section">
-                <v-btn style="background-color: rgb(253,185,19); color: white;" elevation="2">분석</v-btn>
-            </div>
-        </div>
-        <br>
+      <div>
+        <span style="font-size: 25px">지출 </span>
+        <span style="font-size: 30px">600,000원</span>
+      </div>
+      <div>
+        <span style="font-size: 25px">수입 </span>
+        <span style="font-size: 30px">100,000원</span>
+      </div>
+      <div class="anaysis-button-section">
+        <v-btn
+          style="background-color: rgb(253,185,19); color: white;"
+          elevation="2"
+          >분석</v-btn
+        >
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/budget/SpendingPage.vue
+++ src/views/budget/SpendingPage.vue
@@ -3,15 +3,24 @@
     <div>
       지출관리페이지
     </div>
+    <BudgetMonthPicker />
+    <BudgetTopSection />
+    <SpendingBottomSection />
     <BudgetSideBar />
   </div>
 </template>
 
 <script>
 import BudgetSideBar from "@/components/budget/BudgetSideBar.vue";
+import BudgetMonthPicker from "@/components/budget/BudgetMonthPicker.vue";
+import BudgetTopSection from "@/components/budget/BudgetTopSection.vue";
+import SpendingBottomSection from "@/components/budget/SpendingBottomSection.vue";
 export default {
   components: {
     BudgetSideBar,
+    BudgetMonthPicker,
+    BudgetTopSection,
+    SpendingBottomSection,
   },
 };
 </script>
